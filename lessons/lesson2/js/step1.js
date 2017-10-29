6a03ecc18710fa234d80907959d1cff20cf334b5
Stanislav Tyrinov <tyrinow1994@gmail.com>
2017-10-30 00:30:40 +0200

    lesson2_new

---
+++ lessons/lesson2/js/step1.js
@@ -0,0 +1,74 @@
+// Конверсія типів
+// 1.String
+
+// var id = 12345; //Number
+// var url = "http://vk.com/id" + id; // String
+// console.log(url);
+//
+//
+// console.log(String(false));       // false --> "false"
+// console.log(String(null));        // null --> "null"
+// console.log(String(undefined));   //undefind --> "undefined"
+
+
+
+// 2.Number
+
+// string to number
+
+// var str1 = "123";
+// console.log(+str1); // number 123
+// console.log(Number(str1));
+//
+// var str2 = "qwe123";
+// console.log(+str2);// NaN
+//
+//
+//
+// console.log(+undefined);  //NaN
+// console.log(+false);  //0
+// console.log(+true);  // 1
+// console.log(+null);  //0
+
+
+
+
+
+// 3.Boolean
+
+//null, undefined
+// console.log(Boolean(null)); //false
+// console.log(Boolean(undefined)); //false
+//
+//
+// // strings
+// console.log(Boolean("some text")); //true
+// console.log(Boolean("")); //false  (empty)
+//
+// // number
+// console.log(Boolean(123)); //true
+//
+// console.log(Boolean(0)); //false
+// console.log(Boolean(NaN)); //false
+
+
+
+
+
+
+
+// task
+// what will be result?
+// "" + 1 + 0
+// "" - 1 + 0
+// true + false
+// 6 / "3"
+// "2" * "3"
+// 4 + 5 + "px"
+// 4 + (5 + "px")
+// "$" + 4 + 5
+// "4" - 2
+// "4px" - 2
+// 7 / 0
+// null + 1
+// undefined + 1
