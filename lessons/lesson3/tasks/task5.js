f61d0c6b09b6732e1af35f07afee2b2e22a53178
Stanislav Tyrinov <tyrinow1994@gmail.com>
2017-11-01 09:31:31 +0200

    lesson3

---
+++ lessons/lesson3/tasks/task5.js
@@ -0,0 +1,21 @@
+// написати функцію, яка робить наступне
+// в інпут вводимо любе число з нулів і одиниць
+// наприклад 110011
+// і функція має міняти 1 на 0 і навпаки
+// і записувати в змінну result
+// наприклад в інпуті 110011
+// в result 001100
+
+
+
+function someFunc() {
+  var text = document.getElementById("input1").value;
+  var result = "";
+  //тут код
+  // введене значення в змінній text
+
+  
+
+  document.getElementById("input2").value = result;
+}
+document.getElementById("btn").onclick = someFunc;
